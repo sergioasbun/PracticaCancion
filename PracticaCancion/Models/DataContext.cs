4295f55fe5716da6fce6cdd7eec6392897e220d0
sergio asbun fernandez <a2016114282@estudiantes.upsa.edu.bo>
2021-10-11 19:49:04 -0400

    DataContext Agregado

---
+++ PracticaCancion/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticaCancion.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
