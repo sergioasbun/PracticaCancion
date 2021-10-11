02e08928eb209062bf3086ed97b314a121367d5d
sergio asbun fernandez <a2016114282@estudiantes.upsa.edu.bo>
2021-10-11 19:42:18 -0400

    Creacion de modelo

---
+++ PracticaCancion/Models/Cancion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticaCancion.Models
+{
+    public class Cancion
+    {
+        [Key]
+        public string Titulo { get; set; }
+        [Required]
+        public string Autor { get; set; }
+        [Required]
+        public String Letra { get; set; }
+        [Required]
+        public String EnlaceYT { get; set; }
+    }
+}
